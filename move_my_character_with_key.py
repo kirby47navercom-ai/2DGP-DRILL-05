aafac9a3816cfaebd9b8a938a59bd0a98950d943
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-09-25 18:16:00 +0900

    상하좌우로 이동 시키는 handle_events1완성 그리고 상하좌우로 인한 변수 조정 dirx diry를 만들고 각자 받게함

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,41 +5,49 @@ grass = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 
-def dir_check(check):
-    global x,y
+def dir_check():
+    global x,y,dirx, diry
 
-    if check == 0:
-        character.clip_draw(frame * 100, 300, 100, 100, x, y)
-    elif check == 1 or check == -1:
-        x += dir * 5
+    if dirx == 1 or dirx == -1:
+        x += dirx * 10
         character.clip_draw(frame * 100, 100, 100, 100, x, y)
-    elif check == 2 or check == -2:
-        y += dir * 5
+    if diry == 1 or diry == -1:
+        y += diry * 10
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    if dirx ==0 and diry == 0:
+        character.clip_draw(frame * 100, 300, 100, 100, x, y)
 
 def handle_events1():
-    global dir
+    global dirx, diry
     events = get_events()
     for event in events:
         if event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dirx += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dirx -= 1
+            elif event.key == SDLK_UP:
+                diry += 1
+            elif event.key == SDLK_DOWN:
+                diry -= 1
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                dirx -= 1
             elif event.key == SDLK_LEFT:
-                dir += 1
+                dirx += 1
+            elif event.key == SDLK_UP:
+                diry -= 1
+            elif event.key == SDLK_DOWN:
+                diry += 1
 
-def handle_events2():
-    pass
+
 
 
 x = 800 // 2
 y = 180 // 2
 frame = 0
-dir = 0
+dirx = 0
+diry = 0
 
 
 
@@ -49,8 +57,7 @@ while True:
     grass.draw(400, 100)
 
     handle_events1()
-    handle_events2()
-    dir_check(dir)
+    dir_check()
 
     frame= (frame + 1) % 8
     update_canvas()
