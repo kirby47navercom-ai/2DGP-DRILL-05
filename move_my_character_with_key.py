d326923695936576faaf5c1b0171d22401db7ef9
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-09-25 17:37:34 +0900

    애니메이션을 위한 반복문 활용 그리고 배경 넣기

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,10 +1,18 @@
 from pico2d import *
 
 open_canvas()
-grass = load_image('grass.png')
+grass = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 
+x = 800 // 2
+frame = 0
+dir = 0
+while True:
+    clear_canvas()
+    grass.draw(400, 100)
+    update_canvas()
+    delay(0.01)
 
 
 close_canvas()
