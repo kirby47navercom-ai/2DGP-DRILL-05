a1d3e1a77331279c62af2c33924c0758bfd9f087
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-09-25 18:07:15 +0900

    좌우로 이동 시키는 handle_events1 완성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,17 +6,36 @@ character = load_image('animation_sheet.png')
 
 
 def dir_check(check):
-    if check == 0:
-        character.clip_draw(frame * 100, 300, 100, 100, x, 90)
-    elif check != 0:
-        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    global x,y
 
-def handle_events():
-    pass
+    if check == 0:
+        character.clip_draw(frame * 100, 300, 100, 100, x, y)
+    elif check == 1 or check == -1:
+        x += dir * 5
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+    elif check == 2 or check == -2:
+        y += dir * 5
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+
+def handle_events1():
+    global dir
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
 
 
 x = 800 // 2
+y = 180 // 2
 frame = 0
 dir = 0
 
@@ -27,12 +46,12 @@ while True:
     clear_canvas()
     grass.draw(400, 100)
 
-    handle_events()
+    handle_events1()
     dir_check(dir)
 
     frame= (frame + 1) % 8
     update_canvas()
-    delay(0.1)
+    delay(0.01)
 
 
 close_canvas()
